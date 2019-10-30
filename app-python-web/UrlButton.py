2012b81ebe7c94eb47bbe0873e38e1310b08809d
Administrator <17601002613@163.com>
2019-10-30 18:14:02 +0800

    第一次上传

--- app-python-web/UrlButton.py
+++ app-python-web/UrlButton.py
@@ -2,10 +2,9 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome()
-
 driver.get("http://exy.yunxuetang.cn/kng/knowledgecatalogsearch.htm?sf=UploadDate&s=dc&st=null")
 
-if __name__ == "main":
-    x = input("x:")
-    print(x)
+if __name__ == "__main__":
+
+    print(driver.title)
 

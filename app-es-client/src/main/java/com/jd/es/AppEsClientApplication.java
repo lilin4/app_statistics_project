bb3c584caa7ab54746408be4c75d83ac540c0a99
linli <linli1993>
2020-07-09 11:01:48 +0800

    es基础开发

---
+++ app-es-client/src/main/java/com/jd/es/AppEsClientApplication.java
@@ -0,0 +1,13 @@
+package com.jd.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEsClientApplication.class, args);
+	}
+
+}
---
+++ app-es-client/src/test/java/com/jd/es/AppEsClientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jd.es;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEsClientApplicationTests {
+
+
+
+}
